ec7f79ff94bf99df86c2be4d825ff22487dbc53b
Gabriel Lins <gabriellf.glf@gmail.com>
2023-09-07 17:47:04 -0300

    Implemented Inheritance and Optimized Getters/Setters

--- javafxCRUD/src/DTO/FuncionarioDTO.java
+++ javafxCRUD/src/DTO/FuncionarioDTO.java
@@ -1,25 +1,8 @@
 package DTO;
 
-public class FuncionarioDTO {
-	
-	private String nomeFuncionario,
-			departamentoFuncionario;
+public class FuncionarioDTO extends Funcionario {
+
 	private int idFuncionario;
-	public String getNomeFuncionario() {
-		return nomeFuncionario;
-	}
-
-	public void setNomeFuncionario(String nomeFuncionario) {
-		this.nomeFuncionario = nomeFuncionario;
-	}
-
-	public String getDepartamentoFuncionario() {
-		return departamentoFuncionario;
-	}
-
-	public void setDepartamentoFuncionario(String departamentoFuncionario) {
-		this.departamentoFuncionario = departamentoFuncionario;
-	}
 
 	public int getIdFuncionario() {
 		return idFuncionario;
